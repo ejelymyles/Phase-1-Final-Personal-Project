1a63bc48bb02acd0168f4ec946d18fea7a399eb8
Ej Myles <ejelymyles@gmail.com>
2023-07-01 12:53:48 -0700

    working fetch

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.addEventListener(`DOMContentLoaded`, () => console.log('JS file is connected'))
+document.addEventListener(`DOMContentLoaded`, () => fetchData())
+
+function fetchData (){
+    fetch("https://musicbrainz.org/ws/2/recording?query=artist:%22Future%22%20AND%20recording:%22712PM%22&fmt=json")
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+}
