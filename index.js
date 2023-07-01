7c1e18d342254d7b3501671f9bafb5a8e143e893
Ej Myles <ejelymyles@gmail.com>
2023-07-01 14:26:55 -0700

    convert time variable from MS to Minutes

--- index.js
+++ index.js
@@ -5,6 +5,13 @@
 // const explicitOrClean = recordings[0].disambiguation
 // const releaseDate = recordings[0][`first-release-date`].slice(0,4)
 
+/* TIME VARIABLES
+ const songTime = Math.floor(duration / 1000);
+ const minutes = Math.floor(songTime / 60);
+ const seconds = songTime % 60;
+ const formattedTime = `${minutes}:${seconds.toString().padStart(2, `0`)}`; 
+ */
+
 
 document.addEventListener(`DOMContentLoaded`, () => fetchData())
 
@@ -14,19 +21,23 @@ function fetchData (){
     .then((data) => {
         const recordings = data.recordings;
         const artistName = recordings[0][`artist-credit`][0].name; 
-        const songTitle = recordings[0].title
-        const albumTitle = recordings[0].releases[0].title
-        const explicitOrClean = recordings[0].disambiguation
-        const releaseDate = recordings[0][`first-release-date`].slice(0,4)
-        const duration = recordings[0].length
-        const songTime = Math.floor(duration / 1000)
-        console.log(data)
+        const songTitle = recordings[0].title;
+        const albumTitle = recordings[0].releases[0].title;
+        const explicitOrClean = recordings[0].disambiguation;
+        const releaseDate = recordings[0][`first-release-date`].slice(0,4);
+        const duration = recordings[0].length;
+        const songTime = Math.floor(duration / 1000);
+        const minutes = Math.floor(songTime / 60);
+        const seconds = songTime % 60;
+        const formattedTime = `${minutes}:${seconds.toString().padStart(2, `0`)}`; 
+        console.log(data);
         console.log(artistName);
-        console.log(songTitle)
-        console.log(albumTitle)
-        console.log(explicitOrClean)
-        console.log(releaseDate)
-        console.log(songTime)
+        console.log(songTitle);
+        console.log(albumTitle);
+        console.log(explicitOrClean);
+        console.log(releaseDate);
+        console.log(songTime);
+        console.log(formattedTime)
     })
     .catch(error => console.log("oops, this is an error")); 
 }
