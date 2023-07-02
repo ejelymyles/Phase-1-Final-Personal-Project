6d94abc157ef122f23506e5d41417d7d4c7eb394
Ej Myles <ejelymyles@gmail.com>
2023-07-02 15:08:03 -0700

    stack song info details in DOM

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ const seconds = songTime % 60;
 const formattedTime = `${minutes}:${seconds.toString().padStart(2, `0`)}`; 
 */
 
-
 /*
 FUNCTIONS TO BUILD
-1. build a function to add song (details below)
-2. build a function to remove song (details below)
+1. build a function to add & remove songs (details below) - DONE
 3. build a function to add mouseover (details below)
 4. think about more functinoality to add (filter/sort list, song counter)
 */
@@ -77,7 +75,10 @@ function addSong(){
                 const newSong = document.createElement(`li`);
                 newSong.setAttribute(`class`, `tracklistItem`);
                 newSong.setAttribute(`id`, `${songTitle}-item`);
-                newSong.textContent = `${songTitle}, ${artistName}, ${albumTitle}`;
+                songInfo = document.createElement(`div`);
+                songInfo.innerHTML = `${songTitle}<br>${artistName}<br>${albumTitle}`;
+                newSong.appendChild(songInfo); 
+                // newSong.textContent = `${songTitle}\n${artistName}\n${albumTitle}`;
                 
                 const playlistContainer = document.getElementById(`tracklistContainer`)
                 playlistContainer.appendChild(newSong);
@@ -87,6 +88,7 @@ function addSong(){
                 songButton.setAttribute(`id`, `${songTitle}-button`)
                 songButton.innerText = `Remove`;
                 playlistContainer.appendChild(songButton);
+
                 songButton.addEventListener(`click`, (e) => {
                     if (e.target.innerText === `remove`){
                     } newSong.remove()
@@ -98,33 +100,14 @@ function addSong(){
                 breakLine.setAttribute(`class`, `breakLine`); 
                 playlistContainer.appendChild(breakLine);
 
-            
+                songInput.value = ``;
+                artistInput.value =``; 
         })
-        .catch(error => console.log("Sorry, this song is not available")); 
+        .catch(error => alert("Sorry, this song is not available")); 
      }
 )}
 
 
-/* ADD SONG FUNCTION
-- find a way to grab the form element and store in in a variable = Submission Form - DONE
-- listens for a submit on the form (add event listner to form) - DONE
-- when the submit happens, it follows instructions below:
-- takes the song name value AND the Arist Value and stores them in variables = songValue & artistValue -DONE
-- takes those variables and interpolates them into the fetch url - DONE
-- receives the fetch back and turns the response into json - DONE
-- grabs the songTitle, artistName, albumTitle, explicitOrClean, and releaseDate elements stored in variables - DONE
-- creates an element (li or paragraph??) that contains the songTitle, artistName, albumTitle. needs class name = "tracklist item", needs id - "${songTitle} containter" - DONE
-- appends it to the dom in the ul container - store the ul contianer to a variable = tracklistContainer - DONE
-- creates a button with the innertext (remove) and a class of "remove button"
-*/
-
-
-/* ADD REMOVE SONG FUNCTION 
-- add an event listener that listents for clicks anywhere in the DOM
-- if the event target === class name "remove button"
-- then remove the section that contains that songTitle, artistName, albumTitle (maybe this is easier as a P and not a li)
-- make sure it targets the specific tracklist item and not all of them. (target the specific p or li )
-*/
 
 /* ADD MOUSEOVER FUNCTION
 - add an event listener that listens for a mouseover on the image or the song title 
@@ -134,33 +117,4 @@ function addSong(){
 /* ADD MOUSEOUT FUNCTION
 - add event listener that listens for a mousout of an already mousedover element
 - when the mouseout happens, it no longer displays the song duration, realease date, explicit or clean
-
-
-/*
-function fetchData (){
-    fetch("https://musicbrainz.org/ws/2/recording?query=artist:future%22%20AND%20recording:%22i%27m%20dat%20nigga%22&fmt=json")
-    .then((resp) => resp.json())
-    .then((data) => {
-        const recordings = data.recordings;
-        const artistName = recordings[0][`artist-credit`][0].name; 
-        const songTitle = recordings[0].title;
-        const albumTitle = recordings[0].releases[0].title;
-        const explicitOrClean = recordings[0].disambiguation;
-        const releaseDate = recordings[0][`first-release-date`].slice(0,4);
-        const duration = recordings[0].length;
-        const songTime = Math.floor(duration / 1000);
-        const minutes = Math.floor(songTime / 60);
-        const seconds = songTime % 60;
-        const formattedTime = `${minutes}:${seconds.toString().padStart(2, `0`)}`; 
-        console.log(data);
-        console.log(artistName);
-        console.log(songTitle);
-        console.log(albumTitle);
-        console.log(explicitOrClean);
-        console.log(releaseDate);
-        console.log(formattedTime)
-    })
-    .catch(error => console.log("Sorry, this song is not available")); 
-}
 */
-
